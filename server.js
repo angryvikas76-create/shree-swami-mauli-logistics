265a653738156ddfd71edd0808c4b2ee520f144c
angryvikas76-create <angryvikas76@gmail.com>
2026-06-28 23:39:01 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,3 +5,5 @@ connectDB();
 app.listen(process.env.PORT || 5000, () => {
     console.log(`🚀 Server running on port ${process.env.PORT || 5000}`);
 });
+
+app.use("/uploads", express.static("uploads"));
