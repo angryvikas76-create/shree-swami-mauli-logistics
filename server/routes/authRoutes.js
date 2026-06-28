2c73fedd9f9ca23bdea7b3ba4dc23b4a5e55df9e
angryvikas76-create <angryvikas76@gmail.com>
2026-06-28 23:26:22 +0530

    Update authRoutes.js

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,62 +1,11 @@
 const express = require("express");
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
 
 const router = express.Router();
 
-const User = require("../models/User");
+const authController = require("../controllers/authController");
 
-// Admin Login
-router.post("/login", async (req, res) => {
+router.post("/register", authController.register);
 
-    try {
-
-        const { email, password } = req.body;
-
-        const user = await User.findOne({ email });
-
-        if (!user) {
-            return res.status(401).json({
-                success: false,
-                message: "User not found"
-            });
-        }
-
-        const match = await bcrypt.compare(password, user.password);
-
-        if (!match) {
-            return res.status(401).json({
-                success: false,
-                message: "Invalid Password"
-            });
-        }
-
-        const token = jwt.sign(
-            {
-                id: user._id,
-                role: user.role
-            },
-            process.env.JWT_SECRET,
-            {
-                expiresIn: "7d"
-            }
-        );
-
-        res.json({
-            success: true,
-            token,
-            user
-        });
-
-    } catch (err) {
-
-        res.status(500).json({
-            success: false,
-            message: err.message
-        });
-
-    }
-
-});
+router.post("/login", authController.login);
 
 module.exports = router;
