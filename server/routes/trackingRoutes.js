9a95a4e42428615d09700a0e1c09e60cfa2c482d
angryvikas76-create <angryvikas76@gmail.com>
2026-06-28 23:28:56 +0530

    Update trackingRoutes.js

--- server/routes/trackingRoutes.js
+++ server/routes/trackingRoutes.js
@@ -1,52 +1,9 @@
 const express = require("express");
-const Booking = require("../models/Booking");
 
 const router = express.Router();
 
-// Track Shipment
-router.get("/:bookingId", async (req, res) => {
+const trackingController = require("../controllers/trackingController");
 
-    try {
-
-        const booking = await Booking.findOne({
-            bookingId: req.params.bookingId
-        });
-
-        if (!booking) {
-            return res.status(404).json({
-                success: false,
-                message: "Shipment Not Found"
-            });
-        }
-
-        res.json({
-            success: true,
-            shipment: {
-                bookingId: booking.bookingId,
-                senderName: booking.senderName,
-                receiverName: booking.receiverName,
-                pickupAddress: booking.pickupAddress,
-                deliveryAddress: booking.deliveryAddress,
-                parcelType: booking.parcelType,
-                numberOfBoxes: booking.numberOfBoxes,
-                weight: booking.weight,
-                length: booking.length,
-                width: booking.width,
-                height: booking.height,
-                status: booking.status,
-                bookingDate: booking.createdAt
-            }
-        });
-
-    } catch (err) {
-
-        res.status(500).json({
-            success: false,
-            message: err.message
-        });
-
-    }
-
-});
+router.get("/:bookingId", trackingController.trackShipment);
 
 module.exports = router;
