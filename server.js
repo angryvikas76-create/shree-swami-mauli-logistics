2e2dcf683a5793c436876653523852cbb560972e
angryvikas76-create <angryvikas76@gmail.com>
2026-06-28 23:44:08 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,3 +9,5 @@ app.listen(process.env.PORT || 5000, () => {
 app.use("/uploads", express.static("uploads"));
 
 app.use("/api/upload", require("./routes/uploadRoutes"));
+
+app.use("/api/invoice", require("./routes/invoiceRoutes"));
