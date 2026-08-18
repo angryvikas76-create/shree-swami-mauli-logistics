389934ec58002332f42f6c79e3d929571dd8bb8f
angryvikas76-create <angryvikas76@gmail.com>
2026-08-18 08:33:41 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,11 @@
+require("dotenv").config();
+
+const express = require("express");
 const connectDB = require("./config/db");
+
+const app = express();
+
+app.use(express.json());
 
 connectDB();
 
