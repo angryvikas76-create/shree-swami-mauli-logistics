c3b63d285960e6f9624d5cbc5271618c4b631735
angryvikas76-create <angryvikas76@gmail.com>
2026-09-15 23:26:07 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,6 +7,24 @@ const app = express();
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    res.header(
+        "Access-Control-Allow-Methods",
+        "GET, POST, PUT, PATCH, DELETE, OPTIONS"
+    );
+
+    if (req.method === "OPTIONS") {
+        return res.sendStatus(204);
+    }
+
+    next();
+});
+
 connectDB();
 
 app.listen(process.env.PORT || 5000, () => {
